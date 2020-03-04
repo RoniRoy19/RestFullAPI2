5c6b93f06722e18621c97f160c31c6748111d971
Roni Roy <ronirucse1718@gmail.com>
2020-03-04 12:07:55 +0600

    One file is Edited

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ mongoose.connect(DB, {
 
 // default route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Humayun Product app"});
+    res.json({"message": "Welcome to Roni Singha Roy Product app"});
 });
 
 // listen on port 3000
